545e804bdab6570489072cdcbe8e3d159ef1a1d5
ugur korkmaz <ugurkorkmaz@windowslive.com>
2024-06-13 19:00:53 +0200

    with ajax getList

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,43 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Ugur"
+            },
+             new WriterClass
+            {
+                Id=2,
+                Name="Kübra"
+            },
+              new WriterClass
+            {
+                Id=3,
+                Name="Onur"
+            }
+        };
+    }
+}
